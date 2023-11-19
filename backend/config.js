c48345832f509f8a88e013410a79509669e64ae7
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2023-11-19 23:12:51 +0530

    updated first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://root:' + encodeURIComponent('iitbombayH@7') + '@book-store-mern.aevnell.mongodb.net/books-collecion?retryWrites=true&w=majority';
